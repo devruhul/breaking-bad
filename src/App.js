f88f29c67b9d04ed32cfca55384d0cdfab40e6ca
devruhul <ruhulamin262001@gmail.comm>
2021-12-09 01:00:38 +0600

    all files are updated and final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Header from './components/Homepage/Header';
+import Charactergrid from './components/Character/Charactergrid';
 import './App.css';
+import axios from 'axios';
+import Search from './components/Homepage/Search';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header></Header>
+      <Search getQuery={(q) => { setQuery(q) }}></Search>
+      <Charactergrid items={items} isLoading={isLoading} ></Charactergrid>
     </div>
   );
 }
---
+++ src/components/Character/Charactergrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CharacterItem from './CharacterItem';
+import Spinner from '../Homepage/Spinner'
+const Charactergrid = ({ items, isLoading }) => {
+    return isLoading ? (<Spinner></Spinner>) : (<section className="cards">
+        {items.map(item => (
+            <CharacterItem key={item.char_id} item={item}> </CharacterItem>
+        ))
+        }
+    </section >)
+};
+
+export default Charactergrid;
